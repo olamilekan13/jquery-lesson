576cc18ecb5a626656f94f9a10dcee7070476037
Olamilekan <olamilekanomosanya@gmail.com>
2023-06-19 13:48:32 +0100

    add document.ready function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("h1").click(function() {
+    alert("This is a heading.");
+    alert("I told you, THIS IS A HEADING!");
+  });
+  
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+
+});
+
